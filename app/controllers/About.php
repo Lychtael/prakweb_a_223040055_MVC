bfd84807e2623772ac67efbf2e9244d3c97a90f3
Lychtael <jitabilhaq12@gmail.com>
2024-10-23 12:31:45 +0700

    Video 4 - Controller

---
+++ app/controllers/About.php
@@ -0,0 +1,9 @@
+<?php
+class About {
+    public function index($nama = 'jita', $pekerjaan = 'gamer') {
+        echo "TESTING TESTING $nama, TESTING $pekerjaan";
+    }
+    public function page() {
+        echo 'about/page'; 
+    }
+}
