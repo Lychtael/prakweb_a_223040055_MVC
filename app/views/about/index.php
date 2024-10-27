c11693e185da9992843b16a5b1731ba5c2ac5602
Lychtael <jitabilhaq12@gmail.com>
2024-10-27 23:40:00 +0700

    Video 6 - Assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,9 @@
-    <h1>About</h1>
-    <p>Hello, the name is <?= $data['nama'];?>, age is <?=  $data['umur'];?> and i am <?= $data['pekerjaan'];?></p>
+<div class="container">
+<div class="card text-bg-dark">
+  <img src="<?= BASEURL;?>/img/img1.png" class="card-img" alt="...">
+  <div class="card-img-overlay">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis provident quod iure totam voluptatem. Cum suscipit placeat architecto rerum repellendus, temporibus et maxime eveniet laudantium tenetur a deleniti aspernatur dolorem!</p>
+    <p class="card-text"><small>card text</small></p>
+  </div>
+</div></div>
